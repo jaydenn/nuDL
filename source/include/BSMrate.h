3e975e981565d8a49ad78122e489da032f72debf
Jayden Newstead <jnewstea@asu.edu>
2016-07-27 21:54:29 -0700

    updated header

--- source/include/BSMrate.h
+++ source/include/BSMrate.h
@@ -5,5 +5,5 @@
 double BSMrate(double ErKeV, paramList *pList, int detj);
 
 //rates computed with interpolation
-double intBSMrate(double Er_min, double Er_max, paramList *pList, int detj);
-double diffBSMrate(double Er, paramList *pList, int detj);
+double intBSMrate(double Er_min, double Er_max, paramList *pList, int detj, double sigNorm);
+double diffBSMrate(double Er, paramList *pList, int detj, double sigNorm);
