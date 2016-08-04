3c3d4b5442434f26a11b98b48e85506de205bc57
Jayden Newstead <jnewstea@asu.edu>
2016-08-04 16:19:10 -0700

    removing some old unused files

--- source/include/detectors.h
+++
@@ -1,8 +0,0 @@
-//detector background integrated between two energies in events/t/year
-double intBgRate(detector det, double Er_min, double Er_max);         
-
-//differential background rate /keV/t/year
-double diffBgRate(detector det, double Er);                            
-
-//for declaring a new detector
-int newDetector(detector *det, char *name, double exp, int ndet);
